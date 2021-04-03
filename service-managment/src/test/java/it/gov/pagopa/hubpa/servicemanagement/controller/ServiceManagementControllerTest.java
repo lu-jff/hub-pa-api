5cf5348cde54fa0b8b968a807826c40553d7e2a5
Filippo Marchetti <f.marchetti@rjcsoft.it>
2021-04-03 10:32:33 +0200

    Deleted comments on JUNIT test

--- service-managment/src/test/java/it/gov/pagopa/hubpa/servicemanagement/controller/ServiceManagementControllerTest.java
+++ service-managment/src/test/java/it/gov/pagopa/hubpa/servicemanagement/controller/ServiceManagementControllerTest.java
@@ -17,9 +17,6 @@ import it.gov.pagopa.hubpa.servicemanagement.model.ServiceConfiguratedModel;
 import it.gov.pagopa.hubpa.servicemanagement.model.TributeServiceModel;
 import it.gov.pagopa.hubpa.servicemanagement.service.ServiceManagementService;
 
-
-
-
 @ExtendWith(MockitoExtension.class)
 class ServiceManagementControllerTest {
 
@@ -34,19 +31,8 @@ class ServiceManagementControllerTest {
 	when(serviceManagementService.isServiceConfigurated(any(Long.class))).thenReturn(Boolean.TRUE);
 	ServiceConfiguratedModel serviceConfiguratedModel = serviceManagementController.isServiceConfigurated(1L);
 	assertThat(serviceConfiguratedModel.getResult()).isTrue();
-	
-	/*List<IdpEntry> lista=new ArrayList<>();
-	IdpEntry idpEntry=new IdpEntry();
-	idpEntry.setEntityId("entityIdTest");
-	lista.add(idpEntry);
-
-	when(spidIntegrationService.getAllIdpEntry()).thenReturn(lista);
-
-	InstallmentModel spidProviders = spidSpringRestController.listIdProviders();
-
-	assertThat(spidProviders.getExtraInfo().get(0).getTitle()).isEqualTo("Maggiori informazioni");
-	assertThat(spidProviders.getIdentityProviders().get(0).getEntityId()).isEqualTo("entityIdTest");*/
     }
+
     @Test
     void getServiceTest() throws ServletException {
 	TributeServiceModel modelMock = TributeServiceModelMock.getMock();
@@ -54,39 +40,13 @@ class ServiceManagementControllerTest {
 	TributeServiceModel tributeServiceModel = serviceManagementController.getService(5L);
 	assertThat(tributeServiceModel.getDenomination()).isEqualTo("TariTefa2021");
     }
+
     @Test
     void saveServiceTest() throws ServletException {
 	when(serviceManagementService.saveService(any(TributeServiceModel.class))).thenReturn(Boolean.TRUE);
-	ServiceConfiguratedModel serviceConfiguratedModel = serviceManagementController.saveService(new TributeServiceModel());
+	ServiceConfiguratedModel serviceConfiguratedModel = serviceManagementController
+		.saveService(new TributeServiceModel());
 	assertThat(serviceConfiguratedModel.getResult()).isTrue();
     }
-    //@Test
-    //void authRequestTest() throws ServletException {
 
-	/*ReflectionTestUtils.setField(spidSpringRestController, "assertionConsumerServiceIndex", 0);
-	
-	TributeServiceModel aa = new TributeServiceModel();
-	aa.setDestinationUrl("destinationUrlTest");
-	
-	when(spidIntegrationService.buildAuthenticationRequest(any(String.class),any(Integer.class))).thenReturn(aa);
-
-	TributeServiceModel spidProviders = spidSpringRestController.authRequest("test");
-
-	assertThat(spidProviders.getDestinationUrl()).isEqualTo("destinationUrlTest");
-	*/
-    //}
-
-   // @Test
-   // void decodeResponseExceptionTest() throws ServletException
-   // {
-	/*MockHttpServletRequest request = new MockHttpServletRequest();
-	MockHttpServletResponse response = new MockHttpServletResponse();
-	
-	when(spidIntegrationService.processAuthenticationResponse(any(HttpServletRequest.class),
-		any(HttpServletResponse.class))).thenThrow(NullPointerException.class);
-	assertThatCode(() -> {
-	     spidSpringRestController.decodeResponse("sasas", request, response);
-	}).hasCauseExactlyInstanceOf(NullPointerException.class);
-	*/
-  //  }
 }
