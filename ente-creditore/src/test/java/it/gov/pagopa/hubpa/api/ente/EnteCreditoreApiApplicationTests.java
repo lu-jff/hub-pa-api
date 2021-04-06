d0a68d2e3826cadef29dd7e571769ce3432a7e0e
Giovanni Mancini <g.mancini.0gmail.com>
2021-04-06 09:59:47 +0200

    [PTT-51] Renamed test suite

---
+++ ente-creditore/src/test/java/it/gov/pagopa/hubpa/api/ente/EnteCreditoreApiApplicationTests.java
@@ -0,0 +1,38 @@
+package it.gov.pagopa.hubpa.api.ente;
+
+// import static org.assertj.core.api.Assertions.assertThat;
+// import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+// import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+// import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+// import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+
+@WebMvcTest
+class EnteCreditoreApiApplicationTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private EnteCreditoreService enteCreditoreService;
+
+	@Test
+	public void shouldGetEnte() throws Exception {
+		this.mockMvc.perform(get("/ente/tttt")).andExpect(status().isOk());
+	}
+
+	@Test
+	public void shouldNotGetEnte() throws Exception {
+		this.mockMvc.perform(get("/entxe/tttt")).andExpect(status().is(404));
+	}
+
+}
