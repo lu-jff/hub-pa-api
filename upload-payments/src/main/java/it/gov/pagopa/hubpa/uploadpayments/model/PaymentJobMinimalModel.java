86e2e9285e277d8f393472c079008518403ba845
Filippo Marchetti <f.marchetti@rjcsoft.it>
2021-04-21 12:18:21 +0200

    [PTT-115] add endpoint for update job information

---
+++ upload-payments/src/main/java/it/gov/pagopa/hubpa/uploadpayments/model/PaymentJobMinimalModel.java
@@ -0,0 +1,15 @@
+package it.gov.pagopa.hubpa.uploadpayments.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PaymentJobMinimalModel {
+
+    private Integer nRecordFound;
+    private Integer nRecordAdded;
+    private Integer status;
+}
