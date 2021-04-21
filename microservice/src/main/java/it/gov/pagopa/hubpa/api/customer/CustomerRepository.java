5235023c72bc79c9c126e9282d14d6ba3f5bd541
Filippo Marchetti <f.marchetti@rjcsoft.it>
2021-04-21 17:28:38 +0200

    [PTT-115] removed microservice test project

--- microservice/src/main/java/it/gov/pagopa/hubpa/api/customer/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package it.gov.pagopa.hubpa.api.customer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-}
