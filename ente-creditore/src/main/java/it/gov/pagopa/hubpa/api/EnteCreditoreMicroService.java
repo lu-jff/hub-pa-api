117593d8bf3c092c02f544889f878746bbeb3531
Filippo Marchetti <f.marchetti@rjcsoft.it>
2021-04-27 11:24:15 +0200

    [PTT-53] added swagger to ente-creditore microservice

--- ente-creditore/src/main/java/it/gov/pagopa/hubpa/api/EnteCreditoreMicroService.java
+++ ente-creditore/src/main/java/it/gov/pagopa/hubpa/api/EnteCreditoreMicroService.java
@@ -2,19 +2,43 @@ package it.gov.pagopa.hubpa.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class EnteCreditoreMicroService {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EnteCreditoreMicroService.class, args);
-	}
+    public static void main(String[] args) {
+	SpringApplication.run(EnteCreditoreMicroService.class, args);
+    }
 
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
-	}
+    @Bean
+    public ModelMapper modelMapper() {
+	return new ModelMapper();
+    }
 
+    @Bean
+    public Docket api() {
+	return new Docket(DocumentationType.SWAGGER_2).select()
+		.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class)).paths(PathSelectors.any())
+		.build().apiInfo(apiInfo());
+
+    }
+
+    private ApiInfo apiInfo() {
+	return new ApiInfo("Ente Creditore REST API",
+		"Servizi rest di Gestione ente Creditore per l'applicazione TariTefa", "0.0.1", null, null, null, null,
+		new ArrayList<>());
+
+    }
 }
--- ente-creditore/src/main/java/it/gov/pagopa/hubpa/api/iban/IbanController.java
+++ ente-creditore/src/main/java/it/gov/pagopa/hubpa/api/iban/IbanController.java
@@ -8,8 +8,17 @@ import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
 @RestController
 public class IbanController {
@@ -22,6 +31,7 @@ public class IbanController {
 
   private Logger logger = LoggerFactory.getLogger(IbanController.class);
 
+  @ApiOperation(value = "Recupera la lista degli IBAN di un Ente Creditore", notes = "Recupera la lista degli IBAN di un Ente Creditore", response = List.class)
   @GetMapping(value = "/ente/{codiceFiscaleEnteCreditore}/iban")
   public List<IbanOnlyDto> getIbanByEnteCreditore(@PathVariable("codiceFiscaleEnteCreditore") String codiceFiscaleEc) {
     logger.info("GET IBAN");
@@ -34,10 +44,11 @@ public class IbanController {
 
   }
 
+  @ApiOperation(value = "Crea un IBAN", notes = "Crea un IBAN", response = IbanDto.class)
   @PostMapping(value = "/ente/iban")
   @ResponseStatus(HttpStatus.CREATED)
   @ResponseBody
-  public IbanDto createIbanPost(@RequestBody IbanDto ibanDto) {
+  public IbanDto createIbanPost(@ApiParam(value = "Modello dell'IBAN", required = true) @RequestBody IbanDto ibanDto) {
     IbanEntity ibanCreated = ibanService.create(modelMapper.map(ibanDto, IbanEntity.class));
 
     return modelMapper.map(ibanCreated, IbanDto.class);
