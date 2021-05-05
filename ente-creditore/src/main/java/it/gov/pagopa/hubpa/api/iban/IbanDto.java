78b33a2427ae04ce10760bef12a1674c56f86162
Filippo Marchetti <f.marchetti@rjcsoft.it>
2021-05-05 16:18:46 +0200

    [PTT-63] add Ente creditore service validation

--- ente-creditore/src/main/java/it/gov/pagopa/hubpa/api/iban/IbanDto.java
+++ ente-creditore/src/main/java/it/gov/pagopa/hubpa/api/iban/IbanDto.java
@@ -2,6 +2,9 @@ package it.gov.pagopa.hubpa.api.iban;
 
 import java.util.Date;
 
+import javax.validation.constraints.NotEmpty;
+
+import it.gov.pagopa.hubpa.api.annotation.validation.IvaCode;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,8 +14,12 @@ public class IbanDto {
 
   private Long id;
   private String codAmm;
+  @NotEmpty
   private String denominazioneEnte;
+  @IvaCode
+  @NotEmpty
   private String codiceFiscale;
+  @NotEmpty
   private String iban;
   private String idSellerBank;
   private String stato;
