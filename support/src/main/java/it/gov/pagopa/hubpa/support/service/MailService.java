7da6ce29c5afb3afa266a59528a0bd3279633ed1
Mirko Rossi <m.rossi@rjcsoft.it>
2021-05-13 16:57:55 +0200

    PTT-134 read mail subject and body templates, add tests, changed port to 8087

--- support/src/main/java/it/gov/pagopa/hubpa/support/service/MailService.java
+++ support/src/main/java/it/gov/pagopa/hubpa/support/service/MailService.java
@@ -1,25 +1,24 @@
 package it.gov.pagopa.hubpa.support.service;
 
-import java.util.Date;
-import java.util.Properties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import javax.mail.Message;
 import javax.mail.Message.RecipientType;
 import javax.mail.Multipart;
 import javax.mail.Session;
-import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
+import java.util.Date;
+import java.util.Properties;
 
 @Service
 public class MailService {
 
-    private Properties properties = new Properties();
+    private final Properties properties = new Properties();
 
     @Value("${global.mail.smtp.host}")
     private String mailSmtpHost;
@@ -38,48 +37,46 @@ public class MailService {
 
     private boolean smtpAuth = false;
 
+    @Autowired
+    private TransportService transportService;
+
     private void initProperties() {
-	properties.put("mail.smtp.host", mailSmtpHost);
-	properties.put("mail.smtp.port", mailSmtpPort);
-	String smtpAuthVal = mailSmtpAuth;
-	if ("true".equals(smtpAuthVal)) {
-	    this.smtpAuth = true;
-	}
-	properties.put("mail.smtp.auth", smtpAuthVal);
+        properties.put("mail.smtp.host", mailSmtpHost);
+        properties.put("mail.smtp.port", mailSmtpPort);
+        String smtpAuthVal = mailSmtpAuth;
+        if ("true".equals(smtpAuthVal)) {
+            this.smtpAuth = true;
+        }
+        properties.put("mail.smtp.auth", smtpAuthVal);
     }
 
     public void send(String from, String to, String cc, String subject, String message, boolean html) throws Exception {
-	this.initProperties();
-	Session session = Session.getInstance(properties);
-	try {
-	    Message mail = new MimeMessage(session);
-	    mail.setFrom(new InternetAddress(from));
-	    mail.setSentDate(new Date());
-	    mail.addRecipients(RecipientType.TO, InternetAddress.parse(to));
-	    if (cc != null && !cc.isEmpty()) {
-		mail.addRecipients(RecipientType.CC, InternetAddress.parse(cc));
-	    }
-	    mail.setSubject(subject);
+        this.initProperties();
+        Session session = Session.getInstance(properties);
+        Message mail = new MimeMessage(session);
+        mail.setFrom(new InternetAddress(from));
+        mail.setSentDate(new Date());
+        mail.addRecipients(RecipientType.TO, InternetAddress.parse(to));
+        if (cc != null && !cc.isEmpty()) {
+            mail.addRecipients(RecipientType.CC, InternetAddress.parse(cc));
+        }
+        mail.setSubject(subject);
 
-	    MimeBodyPart messageBodyPart = new MimeBodyPart();
-	    if (html) {
-		messageBodyPart.setText(message, "utf-8", "html");
-	    } else {
-		messageBodyPart.setText(message);
-	    }
-	    // per file deposistati in un path raggiungibile dall'applicazione
-	    Multipart multipart = new MimeMultipart();
-	    multipart.addBodyPart(messageBodyPart);
-	    mail.setContent(multipart);
+        MimeBodyPart messageBodyPart = new MimeBodyPart();
+        if (html) {
+            messageBodyPart.setText(message, "utf-8", "html");
+        } else {
+            messageBodyPart.setText(message);
+        }
+        // per file deposistati in un path raggiungibile dall'applicazione
+        Multipart multipart = new MimeMultipart();
+        multipart.addBodyPart(messageBodyPart);
+        mail.setContent(multipart);
 
-	    if (smtpAuth) {
-		Transport.send(mail, mailUser, mailPassword);
-	    } else {
-		Transport.send(mail);
-	    }
-	} catch (Exception e) {
-	    throw e;
-	}
+        if (smtpAuth) {
+            transportService.send(mail, mailUser, mailPassword);
+        } else {
+            transportService.send(mail);
+        }
     }
-
 }
---
+++ support/src/test/java/it/gov/pagopa/hubpa/support/service/SupportServiceTest.java
@@ -0,0 +1,43 @@
+package it.gov.pagopa.hubpa.support.service;
+
+import it.gov.pagopa.hubpa.support.controller.SupportController;
+import it.gov.pagopa.hubpa.support.entity.Support;
+import it.gov.pagopa.hubpa.support.mock.SupportMock;
+import it.gov.pagopa.hubpa.support.repository.SupportRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+public class SupportServiceTest {
+    @InjectMocks
+    SupportService supportService;
+
+    @Mock
+    SupportRepository supportRepository;
+
+    @Mock
+    private MailService mailService;
+
+    @Test
+    public void saveTest(){
+        ReflectionTestUtils.setField(supportService, "mailSupportFrom", "from");
+        ReflectionTestUtils.setField(supportService, "mailSupportTo", "to");
+        ReflectionTestUtils.setField(mailService, "mailSmtpHost", "mailSmtpHost");
+        ReflectionTestUtils.setField(mailService, "mailSmtpPort", "mailSmtpPort");
+        ReflectionTestUtils.setField(mailService, "mailSmtpAuth", "mailSmtpAuth");
+        ReflectionTestUtils.setField(mailService, "mailUser", "mailUser");
+        ReflectionTestUtils.setField(mailService, "mailPassword", "mailPassword");
+        Support support = SupportMock.getMock();
+        Support supportCall = SupportMock.getMockCall();
+        Boolean response = supportService.save(support);
+        assertThat(response).isTrue();
+        response = supportService.save(supportCall);
+        assertThat(response).isTrue();
+    }
+}
