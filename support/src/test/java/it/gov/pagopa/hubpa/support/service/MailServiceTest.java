8c6e6b0c509204c1cca204e08cb0632c4f0b8621
Filippo Marchetti <f.marchetti@rjcsoft.it>
2021-05-17 16:05:30 +0200

    [PTT-143] fix smell code

--- support/src/test/java/it/gov/pagopa/hubpa/support/service/MailServiceTest.java
+++ support/src/test/java/it/gov/pagopa/hubpa/support/service/MailServiceTest.java
@@ -1,6 +1,7 @@
 package it.gov.pagopa.hubpa.support.service;
 
-import lombok.SneakyThrows;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -9,43 +10,32 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 
 @ExtendWith(MockitoExtension.class)
-public class MailServiceTest {
+class MailServiceTest {
     @InjectMocks
     MailService mailService;
 
     @Mock
     TransportService transportService;
 
-    @SneakyThrows
     @Test
-    public void sendTest(){
+    void sendTest(){
         ReflectionTestUtils.setField(mailService, "mailSmtpHost", "mailSmtpHost");
         ReflectionTestUtils.setField(mailService, "mailSmtpPort", "mailSmtpPort");
         ReflectionTestUtils.setField(mailService, "mailSmtpAuth", "mailSmtpAuth");
         ReflectionTestUtils.setField(mailService, "mailUser", "mailUser");
         ReflectionTestUtils.setField(mailService, "mailPassword", "mailPassword");
-        mailService.send("user@host.domain", "user@host.domain", "cc", "subject", "message", false);
-    }
-
-    @SneakyThrows
-    @Test
-    public void sendWithAuthTest(){
-        ReflectionTestUtils.setField(mailService, "mailSmtpHost", "mailSmtpHost");
-        ReflectionTestUtils.setField(mailService, "mailSmtpPort", "mailSmtpPort");
+        //Test1
+        assertThatNoException().isThrownBy(() -> { 
+            mailService.send("user@host.domain", "user@host.domain", "cc", "subject", "message", Boolean.FALSE);
+        });
+        //test2
+        assertThatNoException().isThrownBy(() -> { 
+            mailService.send("user@host.domain", "user@host.domain", "cc", "subject", "message", Boolean.TRUE);
+        });
+        //Test3
         ReflectionTestUtils.setField(mailService, "mailSmtpAuth", "true");
-        ReflectionTestUtils.setField(mailService, "mailUser", "mailUser");
-        ReflectionTestUtils.setField(mailService, "mailPassword", "mailPassword");
-        mailService.send("user@host.domain", "user@host.domain", "cc", "subject", "message", false);
-    }
-
-    @SneakyThrows
-    @Test
-    public void sendTestWithHtml(){
-        ReflectionTestUtils.setField(mailService, "mailSmtpHost", "mailSmtpHost");
-        ReflectionTestUtils.setField(mailService, "mailSmtpPort", "mailSmtpPort");
-        ReflectionTestUtils.setField(mailService, "mailSmtpAuth", "mailSmtpAuth");
-        ReflectionTestUtils.setField(mailService, "mailUser", "mailUser");
-        ReflectionTestUtils.setField(mailService, "mailPassword", "mailPassword");
-        mailService.send("user@host.domain", "user@host.domain", "cc", "subject", "message", true);
+        assertThatNoException().isThrownBy(() -> { 
+            mailService.send("user@host.domain", "user@host.domain", "cc", "subject", "message", Boolean.FALSE);
+        });
     }
 }
