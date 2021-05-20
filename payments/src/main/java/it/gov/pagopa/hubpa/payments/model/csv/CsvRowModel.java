19e963af15519cec4f752e3dfbac808e2b27d133
Filippo Marchetti <f.marchetti@rjcsoft.it>
2021-05-20 11:29:49 +0200

    [PTT-152] remove fiscal code validator classes and add test cases

--- payments/src/main/java/it/gov/pagopa/hubpa/payments/model/csv/CsvRowModel.java
+++ payments/src/main/java/it/gov/pagopa/hubpa/payments/model/csv/CsvRowModel.java
@@ -8,7 +8,6 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
-import it.gov.pagopa.hubpa.payments.annotation.validation.FiscalCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -26,8 +25,7 @@ public class CsvRowModel implements Serializable {
     @Size(max=70)
     private String surname;
     @NotEmpty
-    //@Pattern(regexp = "^([A-Z]{6}[0-9LMNPQRSTUV]{2}[ABCDEHLMPRST]{1}[0-9LMNPQRSTUV]{2}[A-Z]{1}[0-9LMNPQRSTUV]{3}[A-Z]{1})$|([0-9]{11})$",message="Codice fiscale errato")
-    @FiscalCode
+    @Pattern(regexp = "^([A-Z]{6}[0-9LMNPQRSTUV]{2}[ABCDEHLMPRST]{1}[0-9LMNPQRSTUV]{2}[A-Z]{1}[0-9LMNPQRSTUV]{3}[A-Z]{1})$|([0-9]{11})$",message="Codice fiscale errato")
     private String fiscalCode;
     private Integer type;
     @NotEmpty
