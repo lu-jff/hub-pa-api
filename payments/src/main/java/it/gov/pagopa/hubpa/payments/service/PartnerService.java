fdada9ee556c0f8ecb008c3df31acb88139eeb44
Simone Infante <simone.infante@pagopa.it>
2021-06-07 18:25:50 +0200

    [PTT-223] Refactoring in partner endpoint with a dedicated service

---
+++ payments/src/main/java/it/gov/pagopa/hubpa/payments/service/PartnerService.java
@@ -0,0 +1,49 @@
+
+package it.gov.pagopa.hubpa.payments.service;
+
+import org.springframework.stereotype.Service;
+
+import it.gov.pagopa.hubpa.payments.model.partner.CtPaymentPA;
+import it.gov.pagopa.hubpa.payments.model.partner.PaGetPaymentReq;
+import it.gov.pagopa.hubpa.payments.model.partner.PaGetPaymentRes;
+import it.gov.pagopa.hubpa.payments.model.partner.PaSendRTReq;
+import it.gov.pagopa.hubpa.payments.model.partner.PaSendRTRes;
+import it.gov.pagopa.hubpa.payments.model.partner.PaVerifyPaymentNoticeReq;
+import it.gov.pagopa.hubpa.payments.model.partner.PaVerifyPaymentNoticeRes;
+import it.gov.pagopa.hubpa.payments.model.partner.StOutcome;
+
+@Service
+public class PartnerService {
+
+	public PaVerifyPaymentNoticeRes paVerifyPaymentNotice(PaVerifyPaymentNoticeReq request) {
+
+		// mock response
+		PaVerifyPaymentNoticeRes result = new PaVerifyPaymentNoticeRes();
+		result.setCompanyName("request.getIdBrokerPSP()");
+		result.setOfficeName("officeName");
+		result.setFiscalCodePA("77777777777");
+		result.setPaymentDescription("payment");
+		result.setOfficeName("officeName");
+		return result;
+	}
+
+	public PaGetPaymentRes paGetPayment(PaGetPaymentReq request) {
+
+		// mock response
+		PaGetPaymentRes result = new PaGetPaymentRes();
+		CtPaymentPA data = new CtPaymentPA();
+		data.setCompanyName("company name");
+		data.setCreditorReferenceId("id");
+		result.setData(data);
+		return result;
+	}
+
+	public PaSendRTRes paSendRT(PaSendRTReq request) {
+
+		// mock response
+		PaSendRTRes result = new PaSendRTRes();
+		result.setOutcome(StOutcome.OK);
+		return result;
+	}
+
+}
