ce42f6f93216425701b75cfc175c6495119cf703
Simone Infante <simone.infante@pagopa.it>
2021-06-07 18:25:50 +0200

    [PTT-223] Refactoring in partner endpoint with a dedicated service

--- payments/src/main/java/it/gov/pagopa/hubpa/payments/config/WebServicesConfig.java
+++ payments/src/main/java/it/gov/pagopa/hubpa/payments/config/WebServicesConfig.java
@@ -12,6 +12,8 @@ import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
 
+import it.gov.pagopa.hubpa.payments.model.partner.ObjectFactory;
+
 @EnableWs
 @Configuration
 public class WebServicesConfig extends WsConfigurerAdapter {
@@ -22,14 +24,14 @@ public class WebServicesConfig extends WsConfigurerAdapter {
         MessageDispatcherServlet servlet = new MessageDispatcherServlet();
         servlet.setApplicationContext(applicationContext);
         servlet.setTransformWsdlLocations(true);
-        return new ServletRegistrationBean<>(servlet, "/paForNode_Service/*");
+        return new ServletRegistrationBean<>(servlet, "/partner/*");
     }
 
     @Bean(name = "partner")
     public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema nodeSchema) {
         DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
         wsdl11Definition.setPortTypeName("paForNode_PortType");
-        wsdl11Definition.setLocationUri("/paForNode_Service");
+        wsdl11Definition.setLocationUri("/partner");
         wsdl11Definition.setTargetNamespace("http://pagopa-api.pagopa.gov.it/pa/paForNode.xsd");
         wsdl11Definition.setSchema(nodeSchema);
         return wsdl11Definition;
@@ -38,5 +40,10 @@ public class WebServicesConfig extends WsConfigurerAdapter {
     @Bean
     public XsdSchema nodeSchema() {
         return new SimpleXsdSchema(new ClassPathResource("xsd/paForNode.xsd"));
+    }
+
+    @Bean
+    public ObjectFactory factory() {
+        return new ObjectFactory();
     }
 }
--- payments/src/main/java/it/gov/pagopa/hubpa/payments/endpoints/PartnerEndpoint.java
+++ payments/src/main/java/it/gov/pagopa/hubpa/payments/endpoints/PartnerEndpoint.java
@@ -4,14 +4,13 @@ import javax.xml.bind.JAXBElement;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 import org.springframework.ws.soap.addressing.server.annotation.Action;
 
-import it.gov.pagopa.hubpa.payments.model.partner.CtPaymentPA;
 import it.gov.pagopa.hubpa.payments.model.partner.ObjectFactory;
 import it.gov.pagopa.hubpa.payments.model.partner.PaGetPaymentReq;
 import it.gov.pagopa.hubpa.payments.model.partner.PaGetPaymentRes;
@@ -19,71 +18,44 @@ import it.gov.pagopa.hubpa.payments.model.partner.PaSendRTReq;
 import it.gov.pagopa.hubpa.payments.model.partner.PaSendRTRes;
 import it.gov.pagopa.hubpa.payments.model.partner.PaVerifyPaymentNoticeReq;
 import it.gov.pagopa.hubpa.payments.model.partner.PaVerifyPaymentNoticeRes;
-import it.gov.pagopa.hubpa.payments.model.partner.StOutcome;
+import it.gov.pagopa.hubpa.payments.service.PartnerService;
 
 @Endpoint
 public class PartnerEndpoint {
 
     private Logger logger = LoggerFactory.getLogger(PartnerEndpoint.class);
 
+    @Autowired
+    private PartnerService partnerService;
+
+    @Autowired
+    private ObjectFactory factory;
+
+    @Action("http://pagopa-api.pagopa.gov.it/service/pa/paForNode/paVerifyPaymentNoticeReq")
     @PayloadRoot(localPart = "paVerifyPaymentNoticeReq", namespace = "http://pagopa-api.pagopa.gov.it/pa/paForNode.xsd")
     @ResponsePayload
     public JAXBElement<PaVerifyPaymentNoticeRes> paVerifyPaymentNotice(
             @RequestPayload JAXBElement<PaVerifyPaymentNoticeReq> request) {
 
         logger.info(" paVerifyPaymentNotice START ");
-
-        // mock response
-        ObjectFactory factory = new ObjectFactory();
-        PaVerifyPaymentNoticeRes result = new PaVerifyPaymentNoticeRes();
-        result.setCompanyName("request.getIdBrokerPSP()");
-        result.setOfficeName("officeName");
-        result.setFiscalCodePA("77777777777");
-        result.setPaymentDescription("payment");
-        result.setOfficeName("officeName");
-        JAXBElement<PaVerifyPaymentNoticeRes> response = factory.createPaVerifyPaymentNoticeRes(result);
-
-        logger.info(" paVerifyPaymentNotice FINISH ");
-        return response;
+        return factory.createPaVerifyPaymentNoticeRes(partnerService.paVerifyPaymentNotice(request.getValue()));
     }
 
+    @Action("http://pagopa-api.pagopa.gov.it/service/pa/paForNode/paGetPaymentReq")
     @PayloadRoot(localPart = "paGetPaymentReq", namespace = "http://pagopa-api.pagopa.gov.it/pa/paForNode.xsd")
     @ResponsePayload
     public JAXBElement<PaGetPaymentRes> paGetPayment(@RequestPayload JAXBElement<PaGetPaymentReq> request) {
 
         logger.info(" paGetPayment START ");
-
-        // mock response
-        ObjectFactory factory = new ObjectFactory();
-        PaGetPaymentRes result = new PaGetPaymentRes();
-
-        CtPaymentPA data = new CtPaymentPA();
-        data.setCompanyName("company name");
-        data.setCreditorReferenceId("id");
-
-        result.setData(data);
-
-        JAXBElement<PaGetPaymentRes> response = factory.createPaGetPaymentRes(result);
-
-        logger.info(" paGetPayment FINISH ");
-        return response;
+        return factory.createPaGetPaymentRes(partnerService.paGetPayment(request.getValue()));
     }
 
+    @Action("http://pagopa-api.pagopa.gov.it/service/pa/paForNode/paSendRTReq")
     @PayloadRoot(localPart = "paSendRTReq", namespace = "http://pagopa-api.pagopa.gov.it/pa/paForNode.xsd")
     @ResponsePayload
     public JAXBElement<PaSendRTRes> paSendRT(@RequestPayload JAXBElement<PaSendRTReq> request) {
 
         logger.info(" paSendRT START ");
-
-        // mock response
-        ObjectFactory factory = new ObjectFactory();
-        PaSendRTRes result = new PaSendRTRes();
-
-        result.setOutcome(StOutcome.OK);
-
-        JAXBElement<PaSendRTRes> response = factory.createPaSendRTRes(result);
-
-        logger.info(" paGetPayment FINISH ");
-        return response;
+        return factory.createPaSendRTRes(partnerService.paSendRT(request.getValue()));
     }
 }
