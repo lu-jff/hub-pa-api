8cf449363ff3d842d208f5b49123652b146dc1a8
Filippo Marchetti <f.marchetti@rjcsoft.it>
2021-06-14 12:42:32 +0200

    [PTT-245] remove stacktrace and fix logger

--- payments/src/main/java/it/gov/pagopa/hubpa/payments/service/PaymentService.java
+++ payments/src/main/java/it/gov/pagopa/hubpa/payments/service/PaymentService.java
@@ -89,7 +89,6 @@ public class PaymentService {
 		    debitorRepository.saveAndFlush(newDebitor);
 		}
 	    } catch (Exception ee) {
-		ee.printStackTrace();
 		nRecordAdded = nRecordAddedPre;
 		nRecordWarning = nRecordWarningPre;
 	    }
