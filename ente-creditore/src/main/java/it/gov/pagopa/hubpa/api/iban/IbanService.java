8e40a41f8ee62884ab1ef126bc538c137b1ab739
Mirko Rossi <m.rossi@rjcsoft.it>
2021-06-16 17:50:25 +0200

    [PTT-256] postal iban

--- ente-creditore/src/main/java/it/gov/pagopa/hubpa/api/iban/IbanService.java
+++ ente-creditore/src/main/java/it/gov/pagopa/hubpa/api/iban/IbanService.java
@@ -3,17 +3,28 @@ package it.gov.pagopa.hubpa.api.iban;
 
 import java.util.List;
 
+import it.gov.pagopa.hubpa.api.enumeration.IbanModeEnum;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class IbanService {
 
+  @Value("${postalAbi}")
+  private String postalAbi;
+
   @Autowired
   private IbanRepository ibanRepository;
 
-  public List<IbanEntity> getByEnteCreditore(String codiceFiscaleEc) {
-    return ibanRepository.findIbanByCodiceFiscale(codiceFiscaleEc);
+  public List<IbanEntity> getByEnteCreditore(String codiceFiscaleEc, String ibanMode) {
+    List<IbanEntity> ibanList = ibanRepository.findIbanByCodiceFiscale(codiceFiscaleEc);
+    if (IbanModeEnum.BANKING.getValue().equals(ibanMode)) {
+      ibanList.removeIf(p -> p.getIban().startsWith(postalAbi, 5));
+    } else if(IbanModeEnum.POSTAL.getValue().equals(ibanMode)) {
+      ibanList.removeIf(p -> !p.getIban().startsWith(postalAbi, 5));
+    }
+    return ibanList;
   }
 
   public IbanEntity create(IbanEntity ibanEntity) {
