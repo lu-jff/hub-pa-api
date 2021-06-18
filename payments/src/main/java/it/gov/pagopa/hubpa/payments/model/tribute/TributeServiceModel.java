4c2f3ad7a8fd99eb98383ea8d9d505efe4fe3c4b
Simone Infante <simone.infante@pagopa.it>
2021-06-18 14:29:04 +0200

    [PPT-241-update-model] add iban validator annotation

--- payments/src/main/java/it/gov/pagopa/hubpa/payments/model/tribute/TributeServiceModel.java
+++ payments/src/main/java/it/gov/pagopa/hubpa/payments/model/tribute/TributeServiceModel.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import javax.validation.constraints.Pattern;
 
+import it.gov.pagopa.hubpa.payments.annotation.validation.Iban;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,23 +16,22 @@ import lombok.Setter;
 public class TributeServiceModel implements Serializable {
 
     private static final long serialVersionUID = -9090133850672769597L;
-    private static final String IBAN_REGEX = "^(?:(?:IT|SM)\\d{2}[A-Z]\\d{22}|CY\\d{2}[A-Z]\\d{23}|NL\\d{2}[A-Z]{4}\\d{10}|LV\\d{2}[A-Z]{4}\\d{13}|(?:BG|BH|GB|IE)\\d{2}[A-Z]{4}\\d{14}|GI\\d{2}[A-Z]{4}\\d{15}|RO\\d{2}[A-Z]{4}\\d{16}|KW\\d{2}[A-Z]{4}\\d{22}|MT\\d{2}[A-Z]{4}\\d{23}|NO\\d{13}|(?:DK|FI|GL|FO)\\d{16}|MK\\d{17}|(?:AT|EE|KZ|LU|XK)\\d{18}|(?:BA|HR|LI|CH|CR)\\d{19}|(?:GE|DE|LT|ME|RS)\\d{20}|IL\\d{21}|(?:AD|CZ|ES|MD|SA)\\d{22}|PT\\d{23}|(?:BE|IS)\\d{24}|(?:FR|MR|MC)\\d{25}|(?:AL|DO|LB|PL)\\d{26}|(?:AZ|HU)\\d{27}|(?:GR|MU)\\d{28})$";
 
     private String fiscalCodePrimaryCreditor;
     private String fiscalCodeSecondaryCreditor;
-    @Pattern(regexp = IBAN_REGEX)
+    @Iban
     private String ibanPrimary;
-    @Pattern(regexp = IBAN_REGEX)
+    @Iban
     private String ibanSecondary;
     private String denomination;
     private BigDecimal percentageSecondary;
     private LocalDate dueDateUnique;
     private List<InstallmentModel> installments;
-    @Pattern(regexp = IBAN_REGEX)
+    @Iban
     private String postalIbanPrimary;
     private String postalIbanHolderPrimary;
     private String postalAuthCodePrimary;
-    @Pattern(regexp = IBAN_REGEX)
+    @Iban
     private String postalIbanSecondary;
     private String postalIbanHolderSecondary;
     private String postalAuthCodeSecondary;
