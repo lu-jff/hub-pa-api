32a6345bb2db11b08b0262edf5c97617c934a3ea
Giovanni <g.mancini.0@gmail.com>
2021-06-21 02:30:49 +0200

    [PTT-246] Add tests

--- queue-worker/src/main/java/it/gov/pagopa/hubpa/qworker/converter/CustomMessageConverter.java
+++ queue-worker/src/main/java/it/gov/pagopa/hubpa/qworker/converter/CustomMessageConverter.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectWriter;
 import org.apache.qpid.jms.message.JmsBytesMessage;
 import org.apache.qpid.jms.provider.amqp.message.AmqpJmsMessageFacade;
 import org.apache.qpid.proton.amqp.Symbol;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
 import org.springframework.stereotype.Component;
@@ -18,6 +20,7 @@ public class CustomMessageConverter extends MappingJackson2MessageConverter {
 
     private static final String TYPE_ID_PROPERTY = "_type";
     private static final Symbol CONTENT_TYPE = Symbol.valueOf("application/json");
+    private final Logger logger = LoggerFactory.getLogger(CustomMessageConverter.class);
 
     public CustomMessageConverter() {
         this.setTargetType(MessageType.TEXT);
@@ -27,6 +30,7 @@ public class CustomMessageConverter extends MappingJackson2MessageConverter {
     @Override
     protected BytesMessage mapToBytesMessage(Object object, Session session, ObjectWriter objectWriter)
             throws JMSException, IOException {
+
         final BytesMessage bytesMessage = super.mapToBytesMessage(object, session, objectWriter);
         JmsBytesMessage jmsBytesMessage = (JmsBytesMessage) bytesMessage;
         AmqpJmsMessageFacade facade = (AmqpJmsMessageFacade) jmsBytesMessage.getFacade();
---
+++ queue-worker/src/test/java/it/gov/pagopa/hubpa/qworker/converter/CustomMessageConverterTest.java
@@ -0,0 +1,55 @@
+package it.gov.pagopa.hubpa.qworker.converter;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+
+import javax.jms.JMSException;
+import javax.jms.Session;
+
+import org.apache.qpid.jms.message.JmsBytesMessage;
+import org.apache.qpid.jms.provider.amqp.message.AmqpJmsBytesMessageFacade;
+import org.apache.qpid.proton.amqp.Symbol;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@ExtendWith(MockitoExtension.class)
+public class CustomMessageConverterTest {
+
+  @InjectMocks
+  private CustomMessageConverter customMessageConverter;
+
+  @Mock
+  MappingJackson2MessageConverter converter;
+
+  @Test
+  public void shouldConvertTheMessage() throws JMSException, IOException {
+    ObjectMapper mapper = new ObjectMapper();
+
+    Session mockSession = mock(Session.class);
+
+    AmqpJmsBytesMessageFacade facade = mock(AmqpJmsBytesMessageFacade.class);
+    JmsBytesMessage msg = mock(JmsBytesMessage.class);
+    when(msg.getFacade()).thenReturn(facade);
+    doNothing().when(msg).writeBytes(any());
+
+    when(mockSession.createBytesMessage()).thenReturn(msg);
+    facade.setContentType(Symbol.valueOf("application/json"));
+    customMessageConverter.mapToBytesMessage("fake message", mockSession, mapper.writer());
+
+    verify(msg, times(1)).getFacade();
+
+  }
+
+}
